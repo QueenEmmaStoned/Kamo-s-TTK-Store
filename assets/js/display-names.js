cf48a44724f61c4cea4ec5452b9d12aba3421536
QueenEmmaStoned <vandeven.cookie@gmail.com>
2026-07-09 11:32:31 -0400

    Display Names overwrite

--- assets/js/display-names.js
+++ assets/js/display-names.js
@@ -12,13 +12,7 @@ function humanizeStoreName(text, commandName) {
     let name = shouldUseCommandName ? rawCommandName : rawDisplayName;
 
     name = applyManualCommandSpacing(name);
-
-    name = name
-        .replace(/[_-]+/g, " ")
-        .replace(/([a-z0-9])([A-Z])/g, "$1 $2")
-        .replace(/([A-Z]+)([A-Z][a-z])/g, "$1 $2")
-        .replace(/\s+/g, " ")
-        .trim();
+    name = normalizeNameSpacing(name);
 
     if (shouldUseCommandName) {
         return name;
@@ -38,10 +32,7 @@ function shouldUseCommandNameAsDisplay(rawDisplayName, rawCommandName) {
         rawDisplayName.startsWith("HAR") ||
         rawDisplayName.startsWith("Aya");
 
-    const isHarMeat =
-        /^HAR\s+(CO\s+Race|EL\s+Monster).*Meat$/i.test(rawDisplayName);
-
-    return rawCommandName && (startsWithSpecialPrefix || isHarMeat);
+    return rawCommandName && startsWithSpecialPrefix;
 }
 
 function isHarMeatDisplayName(displayName) {
@@ -118,15 +109,206 @@ function applyManualCommandSpacing(name) {
 
     return name;
 }
-function titleCaseKnownWords(text) {
-    let spaced = splitKnownSuffix(text);
 
-    spaced = spaced
+function normalizeNameSpacing(name) {
+    return name
         .replace(/[_-]+/g, " ")
         .replace(/([a-z0-9])([A-Z])/g, "$1 $2")
         .replace(/([A-Z]+)([A-Z][a-z])/g, "$1 $2")
         .replace(/\s+/g, " ")
         .trim();
+}
+
+function removeDisplayOnlyTags(name) {
+    const tagPhrases = [
+        ["Vanilla", "Expanded"],
+        ["VAE", "Roy"],
+        ["VAE", "Waste"],
+        ["DP", "AT"],
+        ["LWM", "Deep", "Storage"],
+        ["Melee", "Weapon"],
+        ["Gun"],
+        ["Raw"],
+        ["Fish"],
+        ["VAE"],
+        ["AEXP"],
+        ["VPE"],
+        ["VWE"],
+        ["VBE"],
+        ["VCE"],
+        ["VREA"],
+        ["MP"],
+        ["AT"],
+        ["SAB"],
+        ["LWM"],
+        ["DR"],
+        ["DBH"],
+        ["BMOT"],
+        ["GM"],
+        ["SBC"],
+        ["DP"],
+        ["Apparel"],
+        ["Footwear"],
+        ["Headgear"],
+        ["Handwear"],
+        ["Building"],
+        ["Relic"],
+        ["Inert"],
+        ["Mawy"],
+        ["Industrial"]
+    ];
+
+    return removeTagPhrases(name, tagPhrases);
+}
+
+function removeTagPhrases(name, tagPhrases) {
+    let words = name.split(/\s+/).filter(Boolean);
+    let changed = true;
+
+    while (changed) {
+        changed = false;
+
+        for (const phrase of tagPhrases) {
+            for (let i = 0; i <= words.length - phrase.length; i++) {
+                const matches = phrase.every(function (tagWord, offset) {
+                    return words[i + offset] === tagWord;
+                });
+
+                if (matches) {
+                    words.splice(i, phrase.length);
+                    changed = true;
+                    break;
+                }
+            }
+
+            if (changed) {
+                break;
+            }
+        }
+    }
+
+    return words.join(" ").trim();
+}
+
+function movePriorityTagsToFront(name) {
+    const priorityTags = [
+        "Prestige"
+    ];
+
+    let words = name.split(/\s+/);
+    const foundTags = [];
+
+    priorityTags.forEach(function (tag) {
+        const index = words.indexOf(tag);
+
+        if (index !== -1) {
+            foundTags.push(tag);
+            words.splice(index, 1);
+        }
+    });
+
+    if (foundTags.length === 0) {
+        return name;
+    }
+
+    return [...foundTags, ...words].join(" ");
+}
+
+function moveLeadingTagsToEnd(name) {
+    const tagPhrases = [
+        ["Animal", "Vaccine"],
+        ["Vaccine"],
+        ["Targeter"],
+        ["Psytrainer"]
+    ];
+
+    const words = name.split(/\s+/);
+
+    for (const tagWords of tagPhrases) {
+        const startsWithTag = tagWords.every((tagWord, index) => words[index] === tagWord);
+
+        if (startsWithTag && words.length > tagWords.length) {
+            const remainingWords = words.slice(tagWords.length);
+            return [...remainingWords, ...tagWords].join(" ");
+        }
+    }
+
+    return name;
+}
+
+function reorderEggName(name) {
+    const words = name.split(/\s+/);
+
+    const eggIndex = words.indexOf("Egg");
+    const statusIndex = words.findIndex(word =>
+        word === "Fertilized" || word === "Unfertilized"
+    );
+
+    if (eggIndex === -1 || statusIndex === -1) {
+        return name;
+    }
+
+    const status = words[statusIndex];
+
+    const remainingWords = words.filter((word, index) =>
+        index !== eggIndex && index !== statusIndex
+    );
+
+    return [status, ...remainingWords, "Egg"].join(" ");
+}
+
+function moveLeadingClarifierToEnd(name) {
+    const words = name.split(/\s+/);
+
+    if (words.length < 2) {
+        return name;
+    }
+
+    const leadingClarifierPhrases = [
+        ["Mech", "Serum"],
+        ["Nature", "Shrine"]
+    ];
+
+    for (const phrase of leadingClarifierPhrases) {
+        const startsWithPhrase = phrase.every((word, index) => words[index] === word);
+
+        if (startsWithPhrase && words.length > phrase.length) {
+            return [...words.slice(phrase.length), ...phrase].join(" ");
+        }
+    }
+
+    const leadingClarifiers = new Set([
+        "Meat",
+        "Leather",
+        "Wool",
+        "Egg",
+        "Meal",
+        "Trap",
+        "Shell",
+        "Subcore",
+        "Medicine",
+        "Pack",
+        "Sculpture",
+        "Radiator",
+        "Blocks",
+        "Turret",
+        "Pallet",
+        "Crown",
+        "Techprint"
+    ]);
+
+    const firstWord = words[0];
+
+    if (leadingClarifiers.has(firstWord)) {
+        return words.slice(1).join(" ") + " " + firstWord;
+    }
+
+    return name;
+}
+
+function titleCaseKnownWords(text) {
+    let spaced = splitKnownSuffix(text);
+    spaced = normalizeNameSpacing(spaced);
 
     return spaced
         .split(/\s+/)
@@ -176,166 +358,6 @@ function capitalizeFirstLetter(word) {
     }
 
     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-}
-
-function capitalizeFirstAndLastWords(name) {
-    const words = name.split(/\s+/);
-
-    if (words.length === 0) {
-        return name;
-    }
-
-    words[0] = capitalizeFirstLetter(words[0]);
-
-    if (words.length > 1) {
-        const lastIndex = words.length - 1;
-        words[lastIndex] = capitalizeFirstLetter(words[lastIndex]);
-        }
-
-    return words.join(" ");
-    }
-
-function capitalizeFirstLetter(word) {
-    if (!word) {
-        return word;
-    }
-
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
-
-function removeDisplayOnlyTags(name) {
-        const removableTags = new Set([
-            "Apparel",
-            "Armor",
-            "Footwear",
-            "Headgear",
-            "Handwear",
-            "Building",
-            "Relic",
-            "Inert",
-            "Simple",
-            "Mawy",
-            "Industrial"
-        ]);
-    
-        return name
-            .split(/\s+/)
-            .filter(word => !removableTags.has(word))
-            .join(" ")
-            .trim();
-    }
-function movePriorityTagsToFront(name) {
-    const priorityTags = [
-        "Prestige",
-        "VPE",
-        "VAE",
-        "DBH",
-        "VCE",
-        "VREA",
-        "AEXP",
-        "LWM",
-        "VBE",
-        "AT"
-        
-    ];
-
-    let words = name.split(/\s+/);
-    const foundTags = [];
-
-    priorityTags.forEach(function (tag) {
-        const index = words.indexOf(tag);
-
-        if (index !== -1) {
-            foundTags.push(tag);
-            words.splice(index, 1);
-            }
-        });
-
-    if (foundTags.length === 0) {
-        return name;
-        }
-
-    return [...foundTags, ...words].join(" ");
-    }
-
-function moveLeadingTagsToEnd(name) {
-    const tagPhrases = [
-        ["Animal", "Vaccine"],
-        ["Vaccine"]
-    ];
-
-    const words = name.split(/\s+/);
-
-    for (const tagWords of tagPhrases) {
-        const startsWithTag = tagWords.every((tagWord, index) => words[index] === tagWord);
-
-        if (startsWithTag && words.length > tagWords.length) {
-            const remainingWords = words.slice(tagWords.length);
-            return [...remainingWords, ...tagWords].join(" ");
-            }
-        }
-
-    return name;
-    }
-
-function reorderEggName(name) {
-    const words = name.split(/\s+/);
-
-    const eggIndex = words.indexOf("Egg");
-    const statusIndex = words.findIndex(word =>
-        word === "Fertilized" || word === "Unfertilized"
-    );
-
-    if (eggIndex === -1 || statusIndex === -1) {
-        return name;
-    }
-
-    const status = words[statusIndex];
-
-    const remainingWords = words.filter((word, index) =>
-        index !== eggIndex && index !== statusIndex
-    );
-
-    return [status, ...remainingWords, "Egg"].join(" ");
-}
-
-function moveLeadingClarifierToEnd(name) {
-    const words = name.split(/\s+/);
-
-    if (words.length < 2) {
-        return name;
-    }
-
-    const leadingClarifiers = new Set([
-        "Meat",
-        "Leather",
-        "Wool",
-        "Egg",
-        "Meal",
-        "Trap",
-        "Shell",
-        "Subcore",
-        "Medicine",
-        "MechSerum",
-        "Pack",
-        "Sculpture",
-        "Nature Shrine",
-        "Radiator",
-        "Blocks",
-        "Turret",
-        "Pallet",
-        "Crown",
-        "Techprint"
-        
-    ]);
-
-    const firstWord = words[0];
-
-    if (leadingClarifiers.has(firstWord)) {
-        return words.slice(1).join(" ") + " " + firstWord;
-    }
-
-    return name;
 }
 
 window.addEventListener("DOMContentLoaded", function () {
